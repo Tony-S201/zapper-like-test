fe3287f0a64d6fdde50c69fb6a495435957a1e6d
Tony-S201 <swierz.tony@gmail.com>
2022-08-04 22:06:17 +0200

    remove vue cli

--- zapper-like-vue/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-import router from './router'
-import './plugins/element.js'
-import i18n from './i18n'
-
-createApp(App).use(i18n).use(router).use(store).mount('#app')
